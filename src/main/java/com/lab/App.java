d0a30e92ea656315c76435bc141056e5dcbb1a0a
Maulik95lab <maulikforzaplayer@gmail.com>
2025-06-09 08:59:45 +0530

    secomd commit

---
+++ src/main/java/com/lab/App.java
@@ -0,0 +1,13 @@
+package com.lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "SIMPLE MAVEN PROGRAM : 1MV22CS095 - DEVOPS LAB" );
+    }
+}
